bedc80893b4673dce958a95f7ef2400e2ddc7899
Blaine Thigpen <blaineedison@gmail.com>
2017-06-21 12:55:52 -0500

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   get 'welcome/index'
+  get 'homepage' => 'welcome#index'
 
   get 'welcome/about'
+  get 'about' => 'welcome#about'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
